959abf2da5b4232081019a4200d8947d2c8218f9
MasterNomi <33470736+MasterNomi@users.noreply.github.com>
2020-10-23 11:16:56 +0500

    Update scheduler_block_control.hpp
    added scheduler run and get status functions

--- host/include/uhd/rfnoc/scheduler_block_control.hpp
+++ host/include/uhd/rfnoc/scheduler_block_control.hpp
@@ -13,17 +13,27 @@
 
 namespace rfnoc { namespace example {
 
-/*! Block controller for the gain block: Multiply amplitude of signal
+/*! Block controller for the scheduler block: Send control signals to change duc/ddc frequencies.
  *
- * This block multiplies the signal input with a fixed gain value.
+ * This block will send control signal to other blocks.
  */
 class UHD_API scheduler_block_control : public uhd::rfnoc::noc_block_base
 {
 public:
     RFNOC_DECLARE_BLOCK(scheduler_block_control)
 
+    //! The register address of the run 
+    static const uint32_t REG_RUN;
+    
+    /*! Set the run to start/stop scheduling
+     */
+    virtual void scheduer_run(const uint32_t run) = 0;
+    
+    /*! Get the current run value (read it from the device)
+     */
+    virtual uint32_t get_status() = 0;
 };
 
 }} // namespace rfnoc::example
 
-#endif /* INCLUDED_RFNOC_EXAMPLE_GAIN_BLOCK_CONTROL_HPP */
+#endif /* INCLUDED_RFNOC_EXAMPLE_SCHEDULER_BLOCK_CONTROL_HPP */
