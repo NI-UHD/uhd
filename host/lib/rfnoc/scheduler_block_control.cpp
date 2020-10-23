c08faa82e4c4fde2e73ee4c695a197ac1fab867b
MasterNomi <33470736+MasterNomi@users.noreply.github.com>
2020-10-23 11:33:45 +0500

    Update scheduler_block_control.cpp
    simple like gain block

--- host/lib/rfnoc/scheduler_block_control.cpp
+++ host/lib/rfnoc/scheduler_block_control.cpp
@@ -5,7 +5,7 @@
 //
 
 // Include our own header:
-#include <rfnoc/example/scheduler_block_control.hpp>
+#include <uhd/rfnoc/scheduler_block_control.hpp>
 
 // These two includes are the minimum required to implement a block:
 #include <uhd/rfnoc/defaults.hpp>
@@ -14,13 +14,25 @@
 using namespace rfnoc::example;
 using namespace uhd::rfnoc;
 
+const uint32_t scheduler_block_control::REG_RUN = 128;
+
 class scheduler_block_control_impl : public scheduler_block_control
 {
 public:
     RFNOC_BLOCK_CONSTRUCTOR(scheduler_block_control) {}
 
+void scheduer_run(const uint32_t run)
+{
+    regs().poke32(REG_RUN, run);
+}
+
+uint32_t get_status()
+    {
+        return regs().peek32(REG_RUN);
+    }
 
 private:
+
 };
 
 UHD_RFNOC_BLOCK_REGISTER_DIRECT(
